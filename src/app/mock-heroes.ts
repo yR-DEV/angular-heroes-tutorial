a68468bdb3450b1b8b8f9f7fc9122d1e282f6cc5
Zack Claar <claar.zack@gmail.com>
2019-09-02 12:42:59 -0600

    List displays with angular's event binding and conditioning

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'herr' },
+  { id: 12, name: 'herrrrr' },
+  { id: 13, name: 'hurrrr' },
+  { id: 14, name: 'errrrr' },
+  { id: 15, name: 'rrrrrrrr' },
+];
